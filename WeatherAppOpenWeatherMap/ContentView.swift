0a21a8dfb32be73b870e6398729edb7e45b79a13
Alexander Pechinkin <pechinkin04@yandex.ru>
2025-04-21 12:45:38 +0400

    Initial Commit

---
+++ WeatherAppOpenWeatherMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherAppOpenWeatherMap
+//
+//  Created by Александр Печинкин on 21.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAppOpenWeatherMap/WeatherAppOpenWeatherMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppOpenWeatherMapApp.swift
+//  WeatherAppOpenWeatherMap
+//
+//  Created by Александр Печинкин on 21.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppOpenWeatherMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
